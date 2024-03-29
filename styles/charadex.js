950cf261ebb14d433a7ed23b5cb035d11ed55f5a
cheeriko <cheerikola@gmail.com>
2024-03-29 18:00:55 -0500

    More clean up on filters PLEASE

--- styles/charadex.js
+++ styles/charadex.js
@@ -1,142 +1,140 @@
 /* ==================================================================== */
 /* URLs
 ======================================================================= */
-let url = new URL(window.location.href);
-let baseURL = window.location.origin + window.location.pathname;
-let folderURL = window.location.origin + '/' + window.location.pathname.split("/")[1];
+
+    let url = new URL(window.location.href);
+    let baseURL = window.location.origin + window.location.pathname;
+    let folderURL = window.location.origin + '/' + window.location.pathname.split("/")[1];
 
 
 
 /* ==================================================================== */
 /* Load Header and Footer
 ======================================================================= */
-$.get(folderURL + '/includes/header.html', function (data) {$('#header').replaceWith(data);});
-$.get(folderURL + '/includes/footer.html', function (data) {$('#footer').replaceWith(data);});
+
+    $.get(folderURL + '/includes/header.html', function (data) {$('#header').replaceWith(data);});
+    $.get(folderURL + '/includes/footer.html', function (data) {$('#footer').replaceWith(data);});
 
 
 
 /* ==================================================================== */
 /* Clean Sheet Data
 ======================================================================= */
-const scrubData = (sheetData) => {
 
-    cleanJson = JSON.parse(sheetData.substring(47).slice(0, -2));
+    const scrubData = (sheetData) => {
 
-    // Grab column headers
-    const col = [];
-    if (cleanJson.table.cols[0].label) {
-        cleanJson.table.cols.forEach((headers) => {
-            if (headers.label) {
-                col.push(headers.label.toLowerCase().replace(/\s/g, ""));
-            }
+        cleanJson = JSON.parse(sheetData.substring(47).slice(0, -2));
+
+        // Grab column headers
+        const col = [];
+        if (cleanJson.table.cols[0].label) {
+            cleanJson.table.cols.forEach((headers) => {
+                if (headers.label) {
+                    col.push(headers.label.toLowerCase().replace(/\s/g, ""));
+                }
+            });
+        }
+
+        // Scrubs columns and puts them in a readable object
+        const scrubbedData = [];
+        cleanJson.table.rows.forEach((info, num) => {
+            const row = {};
+            const isBoolean = val => 'boolean' === typeof val;
+            col.forEach((ele, ind) => {
+                row[ele] = info.c[ind] != null ?  info.c[ind].f != null && !isBoolean(info.c[ind].v) ? info.c[ind].f : info.c[ind].v != null ? info.c[ind].v : "" : "";
+            });
+            row["orderID"] = num + 1;
+            scrubbedData.push(row);
         });
+
+        // Removes any items that are supposed ot be hidden
+        const publicData = [];
+        scrubbedData.forEach((k, v) => {
+            if(!scrubbedData[v]['hide']) {
+                publicData.push(scrubbedData[v]);
+            };
+        });
+
+        return publicData;
+
     }
-
-    // Scrubs columns and puts them in a readable object
-    const scrubbedData = [];
-    cleanJson.table.rows.forEach((info, num) => {
-        const row = {};
-        const isBoolean = val => 'boolean' === typeof val;
-        col.forEach((ele, ind) => {
-            row[ele] = info.c[ind] != null ?  info.c[ind].f != null && !isBoolean(info.c[ind].v) ? info.c[ind].f : info.c[ind].v != null ? info.c[ind].v : "" : "";
-        });
-        row["orderID"] = num + 1;
-        scrubbedData.push(row);
-    });
-
-    // Removes any items that are supposed ot be hidden
-    const publicData = [];
-    scrubbedData.forEach((k, v) => {
-        if(!scrubbedData[v]['hide']) {
-            publicData.push(scrubbedData[v]);
-        };
-    });
-
-    return publicData;
-
-}
 
 
 
 /* ================================================================ */
 /* Prev/Next in Pagination
 /* ================================================================ */
-let charadexPrevNext = () => {
-    $('.btn-next').on('click', () => { $('.pagination .active').next().children('a')[0].click(); })
-    $('.btn-prev').on('click', () => { $('.pagination .active').prev().children('a')[0].click(); })
-}
+
+    let charadexPrevNext = () => {
+        $('.btn-next').on('click', () => { $('.pagination .active').next().children('a')[0].click(); })
+        $('.btn-prev').on('click', () => { $('.pagination .active').prev().children('a')[0].click(); })
+    }
 
 
 
 /* ================================================================ */
 /* Search Filter
 /* ================================================================ */
-let charadexSearch = (info, searchArray) => {
 
-    let arr = searchArray.map(function(v) {
-        return v.toLowerCase().replace(/\s/g, "");
-    });
+    let charadexSearch = (info, searchArray) => {
 
-    $('#search').on('keyup', () => {
-        let selection = $("#search-filter option:selected").val();
-        let searchString = $('#search').val();
-        if (selection && selection != 'all') {
-            info.search(searchString, [selection]);
-        } else {
-            info.search(searchString, arr);
-        }
-    });
+        let arr = searchArray.map(function(v) {
+            return v.toLowerCase().replace(/\s/g, "");
+        });
 
-};
+        $('#search').on('keyup', () => {
+            let selection = $("#search-filter option:selected").val();
+            let searchString = $('#search').val();
+            if (selection && selection != 'all') {
+                info.search(searchString, [selection]);
+            } else {
+                info.search(searchString, arr);
+            }
+        });
 
-
-
-/* ================================================================ */
-/* Custom Filter
-/* ================================================================ */
-let charadexFilter = (info, filterKey) => {
-    $("#filter").on('change', () => {
-        let selection = $("#filter option:selected").text().toLowerCase();
-        if (selection && !selection.includes('all')) {
-            info.filter(function (i) { return i.values()[filterKey].toLowerCase() == selection; });
-        } else {
-            info.filter();
-        }
-    });
-};
+    };
 
 
 
 /* ================================================================ */
 /* Custom Filter
 /* ================================================================ */
-let keyCreator = (key) => {
-    return key.toLowerCase().replace(/\s/g, "");
-};
+
+    let charadexFilter = (info, filterKey) => {
+        $("#filter").on('change', () => {
+            let selection = $("#filter option:selected").text().toLowerCase();
+            if (selection && !selection.includes('all')) {
+                info.filter(function (i) { return i.values()[filterKey].toLowerCase() == selection; });
+            } else {
+                info.filter();
+            }
+        });
+    };
 
 
 
 /* ================================================================ */
-/* Add All to Array
+/* QOL Funcs
 /* ================================================================ */
-let addAll = (key) => {
-    key.unshift("All")
-    return key;
-};
+
+    let keyCreator = (key) => {
+        return key.toLowerCase().replace(/\s/g, "");
+    };
 
 
+    let addAll = (key) => {
+        key.unshift("All")
+        return key;
+    };
 
-/* ================================================================ */
-/* Add options to a select element
-/* ================================================================ */
-let addOptions = (arr, filter) => {
-    arr.forEach((val) => {
-        let optionHTML = document.createElement('option');
-        optionHTML.value = val.toLowerCase().replace(/\s/g, "");
-        optionHTML.textContent = val;
-        filter.append(optionHTML);
-    });
-};
+    let addOptions = (arr, filter) => {
+        arr.forEach((val) => {
+            let optionHTML = document.createElement('option');
+            optionHTML.value = val.toLowerCase().replace(/\s/g, "");
+            optionHTML.textContent = val;
+            filter.append(optionHTML);
+        });
+    };
 
 
 
@@ -162,7 +160,6 @@ const charadexLarge = (options) => {
         itemAmount: userOptions.itemAmount ? userOptions.itemAmount : 12,
         itemOrder: userOptions.itemOrder ? userOptions.itemOrder : "desc",
         filterCol: userOptions.filterCol ? keyCreator(userOptions.filterCol) : false,
-        filterParams: userOptions.filterParams ? addAll(userOptions.filterParams) : false,
         searchParams: userOptions.searchParams ? addAll(userOptions.searchParams) : false,
         btnFilterCol: userOptions.btnFilterCol ? keyCreator(userOptions.btnFilterCol) : false,
         singleItemParamKey: userOptions.singleItemParamKey ? userOptions.singleItemParamKey : "id",
@@ -321,9 +318,12 @@ const charadexLarge = (options) => {
             /* ================================================================ */
 
             // Add options to filters
-            if (charadexInfo.filterParams) {
-                addOptions(charadexInfo.filterParams, $('#filter'));
-                $('#filter').parent().show();
+            if (charadexInfo.filterCol) {
+                const filterArr = [...new Set(sheetArray.map(i => i[charadexInfo.filterCol]))];
+                if (filterArr > 2) {
+                    addOptions(addAll(filterArr), $('#filter'));
+                    $('#filter').parent().show();
+                }
             }
 
             if (charadexInfo.searchParams) {
@@ -370,8 +370,7 @@ const charadexLarge = (options) => {
 
 
 /* ==================================================================== */
-/* Charadex Simple
-/* This is used for smaller pages like traits and catalogue
+/* Charadex
 ======================================================================= */
 const charadexSmall = (options) => {
 
@@ -380,6 +379,7 @@ const charadexSmall = (options) => {
     /* Options & URL
     ======================================================================= */
     let userOptions = options || {};
+    const urlParams = new URLSearchParams(window.location.search);
 
 
     /* ==================================================================== */
@@ -388,9 +388,10 @@ const charadexSmall = (options) => {
     const charadexInfo = {
         sheetID: userOptions.sheetID ? userOptions.sheetID.includes('/d/') ? userOptions.sheetID.split('/d/')[1].split('/edit')[0] : userOptions.sheetID : "1l_F95Zhyj5OPQ0zs-54pqacO6bVDiH4rlh16VhPNFUc",
         sheetPage: userOptions.sheetPage ? userOptions.sheetPage : "Public Masterlist",
+        itemAmount: userOptions.itemAmount ? userOptions.itemAmount : 12,
         itemOrder: userOptions.itemOrder ? userOptions.itemOrder : "desc",
-        categorizeBy: userOptions.categorizeBy ? userOptions.categorizeBy : "rarity",
-        searchParams: userOptions.searchParams ? userOptions.searchParams : ['item', 'trait'],
+        filterCol: userOptions.filterCol ? keyCreator(userOptions.filterCol) : false,
+        searchParams: userOptions.searchParams ? addAll(userOptions.searchParams) : false,
     };
 
 
@@ -414,8 +415,13 @@ const charadexSmall = (options) => {
         /* ================================================================ */
         let sheetArrayKeys = () => {
 
-            // Grab all keys from a single entry to create an array
+            // Grab all keys from a single entry to create
+            // an array
             let itemArray = Object.keys(sheetArray[0]);
+
+            // Find the index of the cardlink and change
+            // it to something list.js can render
+            itemArray[itemArray.indexOf('cardlink')] = { name: 'cardlink', attr: 'href' };
 
             // Same for images
             itemArray[itemArray.indexOf('image')] = { name: 'image', attr: 'src' };
@@ -428,26 +434,51 @@ const charadexSmall = (options) => {
         /* ================================================================ */
         /* Charadex Gallery
         /* ================================================================ */
+
+        // Add options to filters
+        if (charadexInfo.filterCol) {
+            const filterArr = [...new Set(sheetArray.map(i => i[charadexInfo.filterCol]))];
+            if (filterArr > 2) {
+                addOptions(addAll(filterArr), $('#filter'));
+                $('#filter').parent().show();
+            }
+        }
+
+        if (charadexInfo.searchParams) {
+            addOptions(charadexInfo.searchParams, $('#search-filter'));
+            $('#search-filter').parent().show();
+        }
+
+        // Show Filters
         $('#charadex-filters').show();
 
+        // Create the Gallery
         (() => {
-
             let galleryOptions = {
                 item: 'charadex-entries',
                 valueNames: sheetArrayKeys(),
+                searchColumns: charadexInfo.searchParams,
+                page: charadexInfo.itemAmount,
+                pagination: [{
+                    innerWindow: 1,
+                    left: 1,
+                    right: 1,
+                    item: `<li class='page-item'><a class='page page-link'></a></li>`,
+                    paginationClass: 'pagination-top',
+                }],
             };
 
             let charadex = new List('charadex-gallery', galleryOptions, sheetArray);
 
             // Sort based on ID
-            charadex.sort(charadexInfo.categorizeBy, {order: charadexInfo.categorizeBy})
+            charadex.sort("orderID", { order: charadexInfo.itemOrder, })
 
             // Calling all functions here
-            charadexListFunctions(charadex);
+            charadexFilter(charadex, charadexInfo.filterCol);
+            charadexSearch(charadex, charadexInfo.searchParams);
+            charadexPrevNext();
 
         })();
 
-
     })
-
 };
