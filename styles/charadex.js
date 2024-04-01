7ea8f54361a936fbc3e706f58d96f4dcb8554f0a
cheeriko <cheerikola@gmail.com>
2024-04-01 09:19:06 -0500

    Add staff

--- styles/charadex.js
+++ styles/charadex.js
@@ -260,93 +260,120 @@
 /* Masterlist
 ======================================================================= */
     const masterlist = async (options) => {
-        try {
 
-            // Sort through options
-            const charadexInfo = optionSorter(options);
+        // Sort through options
+        const charadexInfo = optionSorter(options);
 
-            // Fetch sheet data to use
-            const JSON = await fetch(sheetPage(charadexInfo.sheetID, charadexInfo.sheetPage)).then(i => i.text());
+        // Fetch sheet data to use
+        const JSON = await fetch(sheetPage(charadexInfo.sheetID, charadexInfo.sheetPage)).then(i => i.text());
 
-            // Clean up sheet data so we can use it
-            let sheetArray = scrubData(JSON);
+        // Clean up sheet data so we can use it
+        let sheetArray = scrubData(JSON);
 
-            // Grab all our url info
-            const urlParams = new URLSearchParams(window.location.search);
-            let cardKey = Object.keys(sheetArray[0])[0];
-            let preParam = url.search.includes(charadexInfo.fauxFolderColumn) ? '?' + charadexInfo.fauxFolderColumn + '=' + urlParams.get(charadexInfo.fauxFolderColumn) + `&${cardKey}=` : `?${cardKey}=`;
+        // Grab all our url info
+        const urlParams = new URLSearchParams(window.location.search);
+        let cardKey = Object.keys(sheetArray[0])[0];
+        let preParam = url.search.includes(charadexInfo.fauxFolderColumn) ? '?' + charadexInfo.fauxFolderColumn + '=' + urlParams.get(charadexInfo.fauxFolderColumn) + `&${cardKey}=` : `?${cardKey}=`;
 
-            // Create faux folders
-            // Filter through array based on folders
-            if (charadexInfo.fauxFolderColumn) sheetArray = fauxFolderButtons(sheetArray, charadexInfo.fauxFolderColumn, urlParams);
+        // Create faux folders
+        // Filter through array based on folders
+        if (charadexInfo.fauxFolderColumn) sheetArray = fauxFolderButtons(sheetArray, charadexInfo.fauxFolderColumn, urlParams);
 
-            // Reverse baseds on preference
-            charadexInfo.itemOrder == 'asc' ? sheetArray.reverse() : '';
+        // Reverse baseds on preference
+        charadexInfo.itemOrder == 'asc' ? sheetArray.reverse() : '';
 
-            // Add card links to the remaining array
-            for (var i in sheetArray) { sheetArray[i].cardlink = baseURL + preParam + sheetArray[i][cardKey]; }
+        // Add card links to the remaining array
+        for (var i in sheetArray) { sheetArray[i].cardlink = baseURL + preParam + sheetArray[i][cardKey]; }
 
-            // Decide if the url points to profile or entire gallery
-            if (urlParams.has(cardKey)) {
+        // Decide if the url points to profile or entire gallery
+        if (urlParams.has(cardKey)) {
 
-                // Render the prev/next links on profiles
-                prevNextLinks(sheetArray, baseURL, preParam, urlParams, cardKey);
+            // Render the prev/next links on profiles
+            prevNextLinks(sheetArray, baseURL, preParam, urlParams, cardKey);
 
-                // List.js options
-                let itemOptions = {
+            // List.js options
+            let itemOptions = {
+                valueNames: sheetArrayKeys(sheetArray),
+                item: 'charadex-card',
+            };
+
+            // Filter out the right card
+            let singleCard = sheetArray.filter((i) => i[cardKey].includes(urlParams.get(cardKey)))[0];
+
+            // Render card
+            let charadexItem = new List("charadex-gallery", itemOptions, singleCard);
+
+
+        } else {
+
+
+            // Create the Gallery
+            (() => {
+
+                let galleryOptions = {
+                    item: 'charadex-entries',
                     valueNames: sheetArrayKeys(sheetArray),
-                    item: 'charadex-card',
+                    searchColumns: charadexInfo.searchFilterParams,
+                    page: charadexInfo.itemAmount,
+                    pagination: [{
+                        innerWindow: 1,
+                        left: 1,
+                        right: 1,
+                        item: `<li class='page-item'><a class='page page-link'></a></li>`,
+                        paginationClass: 'pagination-top',
+                    }],
                 };
 
-                // Filter out the right card
-                let singleCard = sheetArray.filter((i) => i[cardKey].includes(urlParams.get(cardKey)))[0];
+                // Render Gallery
+                let charadex = new List('charadex-gallery', galleryOptions, sheetArray);
 
-                // Render card
-                let charadexItem = new List("charadex-gallery", itemOptions, singleCard);
+                // Make filters workie
+                charadexFilterSelect(charadex, sheetArray, charadexInfo.filterColumn);
+                charadexSearch(charadex, charadexInfo.searchFilterParams);
 
+                // Show pagination
+                showPagination(sheetArray, charadexInfo.itemAmount);
 
-            } else {
+            })();
 
-
-                // Create the Gallery
-                (() => {
-
-                    let galleryOptions = {
-                        item: 'charadex-entries',
-                        valueNames: sheetArrayKeys(sheetArray),
-                        searchColumns: charadexInfo.searchFilterParams,
-                        page: charadexInfo.itemAmount,
-                        pagination: [{
-                            innerWindow: 1,
-                            left: 1,
-                            right: 1,
-                            item: `<li class='page-item'><a class='page page-link'></a></li>`,
-                            paginationClass: 'pagination-top',
-                        }],
-                    };
-
-                    // Render Gallery
-                    let charadex = new List('charadex-gallery', galleryOptions, sheetArray);
-
-                    // Make filters workie
-                    charadexFilterSelect(charadex, sheetArray, charadexInfo.filterColumn);
-                    charadexSearch(charadex, charadexInfo.searchFilterParams);
-
-                    // Show pagination
-                    showPagination(sheetArray, charadexInfo.itemAmount);
-
-                })();
-
-            }
-
-            // 'Load' the page after the above is said and done
-            loadPage();
-
-        } catch (error) {
-            $('#loading').hide();
-            console.log(error);
-            window.alert(error, error);
         }
+
+        // 'Load' the page after the above is said and done
+        loadPage();
+
+    }; 
+
+
+/* ==================================================================== */
+/* For showing off just cards
+======================================================================= */
+    const cards = async (options) => {
+
+        // Sort through options
+        const charadexInfo = optionSorter(options);
+
+        // Fetch sheet data to use
+        const JSON = await fetch(sheetPage(charadexInfo.sheetID, charadexInfo.sheetPage)).then(i => i.text());
+
+        // Clean up sheet data so we can use it
+        let sheetArray = scrubData(JSON);
+
+        // Create the Gallery
+        (() => {
+
+            let galleryOptions = {
+                item: 'charadex-entries',
+                valueNames: sheetArrayKeys(sheetArray),
+            };
+
+            // Render Gallery
+            let charadex = new List('charadex-gallery', galleryOptions, sheetArray);
+
+        })();
+
+        // 'Load' the page after the above is said and done
+        loadPage();
+
     }; 
 
 
