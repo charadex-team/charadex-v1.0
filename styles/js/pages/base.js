aaaf9bb0acbeaa66ac55c68157f6123d4d4b0cc0
cheeriko <cheerikola@gmail.com>
2025-04-28 11:07:55 -0500

    Meta Fix

--- styles/js/pages/base.js
+++ styles/js/pages/base.js
@@ -11,6 +11,13 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   // Load the nav/footer/ect
   charadex.tools.loadIncludedFiles();
+
+  // Change the document meta
+  document.title = document.title.replace('Charadex', charadex.site.title);
+  document.querySelector('meta[name="title"]').setAttribute("content", document.title);
+  document.querySelector('meta[name="url"]').setAttribute("content", charadex.site.url);
+  document.querySelector('meta[name="description"]').setAttribute("content", charadex.site.description);
+
   charadex.tools.loadPage('#charadex-body', 100);
 
 });
