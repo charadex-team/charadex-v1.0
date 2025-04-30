606e875808497e64a94d951296a21297ad657e16
cheeriko <cheerikola@gmail.com>
2025-04-30 18:18:31 -0500

    Add multi select

--- styles/js/list.js
+++ styles/js/list.js
@@ -121,15 +121,16 @@ charadex.listFeatures.filters = (parameters, selector = 'charadex') => {
       .text(filter);
 
       // Find the select and add the filter name & options
-      newFilter
-      .find('select')
+      newFilter.find('select')
       .attr('name', charadex.tools.scrub(filter))
-      .append(charadex.tools.createSelectOptions(parameters[filter]));
+      .append(charadex.tools.createSelectOptions(parameters[filter]))
+      .selectpicker();
 
       // Add to the filters container
       filtersElement.append(newFilter);
 
     }
+
 
     return true;
 
@@ -152,11 +153,12 @@ charadex.listFeatures.filters = (parameters, selector = 'charadex') => {
         // Get the key from the select name attr
         // And whatever the user selected
         let key = $(this).find('select').attr('name');
-        let selection = $(this).find('option:selected').val();
+        let selection = $(this).find('option:selected').toArray().map(item => item.text);
+
 
         // Filter the list
-        if (selection && selection !== 'all') {
-          listJs.filter(list => charadex.tools.scrub(list.values()[key]) === selection);
+        if (charadex.tools.checkArray(selection) && !selection.includes('all')) {
+          listJs.filter(list => selection.includes(list.values()[key]));
         } else {
           listJs.filter();
         }
