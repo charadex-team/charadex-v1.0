b5b2fd2f8fd5e9ddc56ee87eefc5325b64e1c468
cheeriko <cheerikola@gmail.com>
2025-05-06 18:57:21 -0500

    Prompt background fix

--- styles/js/pages/prompts.js
+++ styles/js/pages/prompts.js
@@ -8,12 +8,17 @@ import { charadex } from '../charadex.js';
 /* Load
 ======================================================================= */
 document.addEventListener("DOMContentLoaded", async () => {
-  let dex = await charadex.initialize.page(null, charadex.page.prompts, null, (data) => {
-    $('.cd-prompt-background').each(function(i) {
-      const element = $(this);
-      const image = data.array[i]?.image;
-      element.attr('style', `background-image: url(${image})`);
-    });
+  let dex = await charadex.initialize.page(null, charadex.page.prompts, null, 
+  (listData) => {
+    let backgroundElement = $('.cd-prompt-background');
+    if (listData.type == 'profile') {
+      backgroundElement.attr('style', `background-image: url(${listData.profileArray[0].image})`);
+    } else {
+      backgroundElement.each(function(i) {
+        const image = listData.array[i]?.image;
+        $(this).attr('style', `background-image: url(${image})`);
+      });
+    }
   });
   charadex.tools.loadPage('.softload', 500);
 });
