4d00d1fe9a89a5af79cdd25617b7ef0dcd87d9dd
cheeriko <cheerikola@gmail.com>
2025-05-08 13:49:25 -0500

    Tweak

--- styles/js/pages/base.js
+++ styles/js/pages/base.js
@@ -6,7 +6,7 @@ import { charadex } from '../utilities.js';
 /* ==================================================================== */
 /* Load
 ======================================================================= */
-document.addEventListener("DOMContentLoaded", async () => {
+document.addEventListener("DOMContentLoaded", () => {
   charadex.tools.loadIncludedFiles();
   charadex.tools.updateMeta();
   charadex.tools.loadPage('#charadex-body', 100);
